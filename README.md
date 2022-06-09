8119eda855223ae1a8b77a5fe11139633b3f3ab9
Alexandre Lamberty <alexandrelamberty@gmail.com>
2022-06-09 21:17:23 +0200

    Updates

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Actions Hugo Notes
 
+Github Actions for formatting my personal notes into Hugo content
 
+## Example usage
+
+```yaml
+	uses: alexandrelamberty/actions-hugo-notes@master
+	with:
+		source: 'notes'
+		destination: 'content'
+```
---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -l
+
+set -e  # if a command fails it stops the execution
+set -u  # script fails if trying to access to an undefined variable
+
+echo "[+] Action start"
+
+SOURCE_DIRECTORY="${1}"
+DESTINATION_DIRECTORY="${2}"
+
+echo " - Source directory: $SOURCE_DIRECTORY"
+echo " - Destination directory: $DESTINATION_DIRECTORY"
